492c386c5bcfc701d5406e276ee9bc03da09ce7e
Thomas Stratmann <thomas.stratmann@9elements.com>
2017-04-10 23:20:28 +0200

    initial commit

---
+++ lib/onartsipac.ex
@@ -0,0 +1,18 @@
+defmodule Onartsipac do
+  @moduledoc """
+  Documentation for Onartsipac.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Onartsipac.hello
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
