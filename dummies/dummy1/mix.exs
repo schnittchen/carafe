05b076605f823af12920e57d1a844e98340fabbd
Thomas Stratmann <thomas.stratmann@9elements.com>
2017-04-16 23:09:57 +0200

    remove unnecessary dep in dummy

--- dummies/dummy1/mix.exs
+++ dummies/dummy1/mix.exs
@@ -29,8 +29,7 @@ defmodule Dummy1.Mixfile do
   # Type "mix help deps" for more examples and options
   defp deps do
     [
-      {:onartsipac, path: "__ONARTSIPAC_BASE__"}, # the path will be replaced in CI
-      {:plug, "~> 1.3"}
+      {:onartsipac, path: "__ONARTSIPAC_BASE__"} # the path will be replaced in CI
     ]
   end
 end
