a066a619b62853bca40758a910fb954be6e3a5d1
Thomas Stratmann <thomas.stratmann@9elements.com>
2017-04-21 22:32:01 +0200

    make dummy depend on edeliver application

--- dummies/dummy1/mix.exs
+++ dummies/dummy1/mix.exs
@@ -15,7 +15,7 @@ defmodule Dummy1.Mixfile do
   # Type "mix help compile.app" for more information
   def application do
     # Specify extra applications you'll use from Erlang/Elixir
-    [extra_applications: [:logger]]
+    [extra_applications: [:logger, :edeliver]]
   end
 
   # Dependencies can be Hex packages:
@@ -29,6 +29,7 @@ defmodule Dummy1.Mixfile do
   # Type "mix help deps" for more examples and options
   defp deps do
     [
+      {:edeliver, "~> 1.4.2"},
       {:onartsipac, path: "__ONARTSIPAC_BASE__"} # the path will be replaced in CI
     ]
   end
