005475f49c113d3fd1be72a42be655153d145f38
Thomas Stratmann <thomas.stratmann@9elements.com>
2017-04-24 23:56:05 +0200

    make shelling out even more readable

--- test/onartsipac_test.exs
+++ test/onartsipac_test.exs
@@ -4,101 +4,83 @@ defmodule OnartsipacTest do
 
   setup %{dummy: dummy} do
     # Kill leftover processes
-    %{status: status} =
-      Porcelain.exec("sudo", ~w{su - user -c} ++ ["pkill -f ^#{dummy.app_path |> Regex.escape}.*run_erl"])
-    if !(status in [0,1]) do
-      raise "failed to pkill old processes"
-    end
+    Enamel.new(as: "user", good_exits: [0, 1])
+    |> Enamel.command([~w{pkill -f}, "^#{dummy.app_path |> Regex.escape}.*run_erl"])
+    |> Enamel.run!
 
     File.rm_rf! dummy.onartsipac_path
     File.mkdir_p! dummy.onartsipac_path
     File.cp_r! ".", dummy.onartsipac_path
     File.rm_rf! Path.join(dummy.onartsipac_path, ".git")
 
-    %{status: 0} =
-      Porcelain.exec("find", ~w<#{dummy.remote} -name mix.exs -exec sed -i
-        s|__ONARTSIPAC_PATH__|#{dummy.onartsipac_path}| {} ;>)
+    Enamel.command([:find, dummy.remote, ~w<-name mix.exs -exec sed -i
+      s|__ONARTSIPAC_PATH__|#{dummy.onartsipac_path}| {} ;>])
+    |> Enamel.run!
 
-    %{status: 0} =
-      Porcelain.exec("git", ~w{-C #{dummy.remote} init})
-
-    %{status: 0} =
-      Porcelain.exec("git", ~w{-C #{dummy.remote} config user.name} ++ ["onartsipac test user"])
-
-    %{status: 0} =
-      Porcelain.exec("git", ~w{-C #{dummy.remote} config user.email onartsipac@example.com})
-
-    %{status: 0} =
-      Porcelain.exec("git", ~w{-C #{dummy.remote} add .})
-
-    %{status: 0} =
-      Porcelain.exec("git", ~w{-C #{dummy.remote} commit -m bogus})
-
-    %{status: 0} =
-      Porcelain.exec("bundle", [], dummy.poptions)
+    Enamel.command([~w{git -C}, dummy.remote, :init])
+    |> Enamel.command([~w{git -C}, dummy.remote, ~w{config user.name}, "onartsipac test user"])
+    |> Enamel.command([~w{git -C}, dummy.remote, ~w{config user.email onartsipac@example.com}])
+    |> Enamel.command([~w{git -C}, dummy.remote, ~w{add .}])
+    |> Enamel.command([~w{git -C}, dummy.remote, ~w{commit -m bogus}])
+    |> Enamel.command([:bundle], dir: dummy.remote)
+    |> Enamel.run!
 
     :ok
   end
 
   test "updating the repo cache", %{dummy: dummy} do
-    Porcelain.exec("bundle", ~w{exec cap --trace production buildhost:repo:update}, dummy.poptions)
-    |> assert_psuccess
-
-    Porcelain.exec("bundle", ~w{exec cap --trace production buildhost:repo:update}, dummy.poptions)
-    |> assert_psuccess
+    Enamel.new(on_failure: &flunk_for_reason/1, dir: dummy.remote)
+    |> Enamel.command(~w{bundle exec cap --trace production buildhost:repo:update})
+    |> Enamel.command(~w{bundle exec cap --trace production buildhost:repo:update})
+    |> Enamel.run!
   end
 
   test "cleaning the build path", %{dummy: dummy} do
-    %{status: 0} =
-      Porcelain.exec("sudo", ~w{su - user -c} ++ ["touch build_path"])
+    Enamel.new(as: "user")
+    |> Enamel.command([:touch, dummy.build_path])
+    |> Enamel.run!
 
-    Porcelain.exec("bundle", ~w{exec cap --trace production buildhost:clean}, dummy.poptions)
-    |> assert_psuccess
+    Enamel.new(on_failure: &flunk_for_reason/1, dir: dummy.remote)
+    |> Enamel.command(~w{bundle exec cap --trace production buildhost:clean})
+    |> Enamel.command(~w{bundle exec cap --trace production buildhost:clean})
+    |> Enamel.run!
 
     assert !File.exists?(dummy.build_path)
-
-    Porcelain.exec("bundle", ~w{exec cap --trace production buildhost:clean}, dummy.poptions)
-    |> assert_psuccess
   end
 
   test "cleaning the build path partially", %{dummy: dummy} do
-    %{status: 0} =
-      Porcelain.exec("sudo", ~w{su - user -c} ++ ["mkdir -p build_path/foo"])
+    Enamel.new(as: "user")
+    |> Enamel.command([~w{mkdir -p}, "#{dummy.build_path}/foo"])
+    |> Enamel.command([~w{mkdir -p}, "#{dummy.build_path}/deps/foo"])
+    |> Enamel.run!
 
-    %{status: 0} =
-      Porcelain.exec("sudo", ~w{su - user -c} ++ ["mkdir -p build_path/deps/foo"])
-
-    Porcelain.exec("bundle", ~w{exec cap --trace production buildhost:clean:keepdeps}, dummy.poptions)
-    |> assert_psuccess
+    Enamel.new(on_failure: &flunk_for_reason/1, dir: dummy.remote)
+    |> Enamel.command(~w{bundle exec cap --trace production buildhost:clean:keepdeps})
+    |> Enamel.command(~w{bundle exec cap --trace production buildhost:clean:keepdeps})
+    |> Enamel.run!
 
     assert File.exists?([dummy.build_path, "deps/foo"] |> Path.join)
     assert !File.exists?([dummy.build_path, "foo"] |> Path.join)
-
-    Porcelain.exec("bundle", ~w{exec cap --trace production buildhost:clean:keepdeps}, dummy.poptions)
-    |> assert_psuccess
   end
 
   test "preparing the build path and compiling", %{dummy: dummy} do
-    Porcelain.exec("bundle", ~w{exec cap --trace production buildhost:prepare_build_path}, dummy.poptions)
-    |> assert_psuccess
-
-    assert File.exists?([dummy.build_path, "mix.exs"] |> Path.join)
-
-    Porcelain.exec("bundle", ~w{exec cap --trace production buildhost:compile}, dummy.poptions)
-    |> assert_psuccess
+    Enamel.new(on_failure: &flunk_for_reason/1, dir: dummy.remote)
+    |> Enamel.command(~w{bundle exec cap --trace production buildhost:prepare_build_path})
+    |> Enamel.command(~w{bundle exec cap --trace production buildhost:compile})
+    |> Enamel.run!
 
     assert File.exists?([dummy.build_path, "_build/prod/lib/dummy1"] |> Path.join)
   end
 
   test "executing mix release", %{dummy: dummy} do
-    %{status: 0} =
-      Porcelain.exec("bundle", ~w{exec cap --trace production buildhost:prepare_build_path}, dummy.poptions)
+    Enamel.new(dir: dummy.remote)
+    |> Enamel.command(~w{bundle exec cap --trace production buildhost:prepare_build_path})
+    |> Enamel.command(~w{bundle exec cap --trace production buildhost:compile})
+    |> Enamel.run!
 
-    %{status: 0} =
-      Porcelain.exec("bundle", ~w{exec cap --trace production buildhost:compile}, dummy.poptions)
-
-    Porcelain.exec("bundle", ~w{exec cap --trace production buildhost:mix:release}, dummy.poptions)
-    |> assert_psuccess
+    Enamel.new(on_failure: &flunk_for_reason/1, dir: dummy.remote)
+    |> Enamel.command(~w{bundle exec cap --trace production buildhost:mix:release})
+    |> Enamel.run!
 
     distillery_env = dummy.name # TODO: make this configurable
     assert File.exists?(
@@ -107,77 +89,57 @@ defmodule OnartsipacTest do
   end
 
   test "downloading a release archive", %{dummy: dummy} do
-    %{status: 0} =
-      Porcelain.exec("bundle", ~w{exec cap --trace production buildhost:prepare_build_path}, dummy.poptions)
+    Enamel.new(dir: dummy.remote)
+    |> Enamel.command(~w{bundle exec cap --trace production buildhost:prepare_build_path})
+    |> Enamel.command(~w{bundle exec cap --trace production buildhost:compile})
+    |> Enamel.command(~w{bundle exec cap --trace production buildhost:mix:release})
+    |> Enamel.run!
 
-    %{status: 0} =
-      Porcelain.exec("bundle", ~w{exec cap --trace production buildhost:compile}, dummy.poptions)
-
-    %{status: 0} =
-      Porcelain.exec("bundle", ~w{exec cap --trace production buildhost:mix:release}, dummy.poptions)
-
-    Porcelain.exec("bundle", ~w{exec cap --trace production buildhost:archive:download}, dummy.poptions)
-    |> assert_psuccess
+    Enamel.new(on_failure: &flunk_for_reason/1, dir: dummy.remote)
+    |> Enamel.command(~w{bundle exec cap --trace production buildhost:archive:download})
+    |> Enamel.run!
 
     assert File.exists?(
       [dummy.remote, "archive.tar.gz"] |> Path.join)
   end
 
   test "uploading and unpacking a release archive", %{dummy: dummy} do
-    %{status: 0} =
-      Porcelain.exec("bundle", ~w{exec cap --trace production buildhost:prepare_build_path}, dummy.poptions)
+    Enamel.new(dir: dummy.remote)
+    |> Enamel.command(~w{bundle exec cap --trace production buildhost:prepare_build_path})
+    |> Enamel.command(~w{bundle exec cap --trace production buildhost:compile})
+    |> Enamel.command(~w{bundle exec cap --trace production buildhost:mix:release})
+    |> Enamel.run!
 
-    %{status: 0} =
-      Porcelain.exec("bundle", ~w{exec cap --trace production buildhost:compile}, dummy.poptions)
-
-    %{status: 0} =
-      Porcelain.exec("bundle", ~w{exec cap --trace production buildhost:mix:release}, dummy.poptions)
-
-    Porcelain.exec("bundle", ~w{exec cap --trace production buildhost:archive:download node:archive:upload_and_unpack}, dummy.poptions)
-    |> assert_psuccess
+    Enamel.new(on_failure: &flunk_for_reason/1, dir: dummy.remote)
+    |> Enamel.command(~w{bundle exec cap --trace production buildhost:archive:download node:archive:upload_and_unpack})
+    |> Enamel.run!
 
     assert File.exists?(
       [dummy.app_path, "bin/#{dummy.name}"] |> Path.join)
   end
 
   test "basic interaction with nodes", %{dummy: dummy} do
-    %{status: 0} =
-      Porcelain.exec("bundle", ~w{exec cap --trace production buildhost:prepare_build_path}, dummy.poptions)
+    Enamel.new(dir: dummy.remote)
+    |> Enamel.command(~w{bundle exec cap --trace production buildhost:prepare_build_path})
+    |> Enamel.command(~w{bundle exec cap --trace production buildhost:compile})
+    |> Enamel.command(~w{bundle exec cap --trace production buildhost:mix:release})
+    |> Enamel.command(~w{bundle exec cap --trace production buildhost:archive:download node:archive:upload_and_unpack})
+    |> Enamel.run!
 
-    %{status: 0} =
-      Porcelain.exec("bundle", ~w{exec cap --trace production buildhost:compile}, dummy.poptions)
+    Enamel.new(on_failure: &flunk_for_reason/1, dir: dummy.remote)
+    |> Enamel.command(~w{bundle exec cap --trace production node:ping}, expect_fail: true)
+    |> Enamel.command(~w{bundle exec cap --trace production node:start})
+    |> Enamel.command(~w{bundle exec cap --trace production node:start})
+    |> Enamel.command(~w{bundle exec cap --trace production node:ping})
+    |> Enamel.command(~w{bundle exec cap --trace production node:stop})
+    |> Enamel.command(~w{bundle exec cap --trace production node:stop}, expect_fail: true)
+    |> Enamel.command(~w{bundle exec cap --trace production node:ping}, expect_fail: true)
+    |> Enamel.command(~w{bundle exec cap --trace production node:full_restart})
+    |> Enamel.command(~w{bundle exec cap --trace production node:full_restart})
+    |> Enamel.run!
+  end
 
-    %{status: 0} =
-      Porcelain.exec("bundle", ~w{exec cap --trace production buildhost:mix:release}, dummy.poptions)
-
-    %{status: 0} =
-      Porcelain.exec("bundle", ~w{exec cap --trace production buildhost:archive:download node:archive:upload_and_unpack}, dummy.poptions)
-
-    Porcelain.exec("bundle", ~w{exec cap --trace production node:ping}, dummy.poptions)
-    |> assert_pfailure
-
-    Porcelain.exec("bundle", ~w{exec cap --trace production node:start}, dummy.poptions)
-    |> assert_psuccess
-
-    Porcelain.exec("bundle", ~w{exec cap --trace production node:start}, dummy.poptions)
-    |> assert_psuccess
-
-    Porcelain.exec("bundle", ~w{exec cap --trace production node:ping}, dummy.poptions)
-    |> assert_psuccess
-
-    Porcelain.exec("bundle", ~w{exec cap --trace production node:stop}, dummy.poptions)
-    |> assert_psuccess
-
-    Porcelain.exec("bundle", ~w{exec cap --trace production node:stop}, dummy.poptions)
-    |> assert_pfailure
-
-    Porcelain.exec("bundle", ~w{exec cap --trace production node:ping}, dummy.poptions)
-    |> assert_pfailure
-
-    Porcelain.exec("bundle", ~w{exec cap --trace production node:full_restart}, dummy.poptions)
-    |> assert_psuccess
-
-    Porcelain.exec("bundle", ~w{exec cap --trace production node:full_restart}, dummy.poptions)
-    |> assert_psuccess
+  def flunk_for_reason(reason) do
+    flunk "Failed with reason: #{reason}"
   end
 end
