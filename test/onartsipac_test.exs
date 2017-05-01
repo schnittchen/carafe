f58c3a0f982a14c02b9eb2cde193212e5bb9442e
Thomas Stratmann <thomas.stratmann@9elements.com>
2017-05-01 22:07:31 +0200

    test for a full deploy of a release

--- test/onartsipac_test.exs
+++ test/onartsipac_test.exs
@@ -115,6 +115,14 @@ defmodule OnartsipacTest do
     |> Enamel.run!
   end
 
+  test "full deploy of a release", %{dummy: dummy} do
+    Enamel.new(on_failure: &flunk_for_reason/1, dir: dummy.capistrano_wd)
+    |> Enamel.command(~w{bundle exec cap --trace production node:ping}, expect_fail: true)
+    |> Enamel.command(~w{bundle exec cap --trace production buildhost:generate_release buildhost:archive:download node:archive:upload_and_unpack node:full_restart})
+    |> Enamel.command(~w{bundle exec cap --trace production node:ping})
+    |> Enamel.run!
+  end
+
   def flunk_for_reason(reason) do
     flunk "Failed with reason: #{reason}"
   end
