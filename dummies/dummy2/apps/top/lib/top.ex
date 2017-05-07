c39b5ad8cc9b28393e1fca1b50e2cc574c4266c1
Thomas Stratmann <thomas.stratmann@9elements.com>
2017-05-07 16:07:38 +0200

    make umbrella applications dependent

--- dummies/dummy2/apps/top/lib/top.ex
+++ dummies/dummy2/apps/top/lib/top.ex
@@ -1,18 +1,22 @@
 defmodule Top do
-  @moduledoc """
-  Documentation for Top.
-  """
+  use Application
 
-  @doc """
-  Hello world.
+  def start(_type, _args) do
+    # asynchronoysly wait for our startup sequence to be finished,
+    # then write started applications to file to be read by test
 
-  ## Examples
-
-      iex> Top.hello
-      :world
-
-  """
-  def hello do
-    :world
+    Agent.start_link fn ->
+      Task.async fn ->
+        Stream.repeatedly(fn ->
+          Application.started_applications
+          |> Enum.map(fn {app, _, _} -> app end)
+        end)
+        |> Stream.drop_while(& !(:top in &1))
+        |> Enum.find(fn _ -> true end)
+        |> Enum.map(&to_string/1)
+        |> Enum.join(" ")
+        |> (& File.write("/tmp/started_applications", &1)).()
+      end
+    end
   end
 end
--- dummies/dummy2/apps/top/mix.exs
+++ dummies/dummy2/apps/top/mix.exs
@@ -19,7 +19,8 @@ defmodule Top.Mixfile do
   # Type "mix help compile.app" for more information
   def application do
     # Specify extra applications you'll use from Erlang/Elixir
-    [extra_applications: [:logger]]
+    [mod: {Top, []},
+     extra_applications: [:logger]]
   end
 
   # Dependencies can be Hex packages:
