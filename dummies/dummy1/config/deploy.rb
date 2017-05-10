4d4d59a1a1764cc5b198c565c816e7d23683b794
Thomas Stratmann <thomas.stratmann@9elements.com>
2017-05-10 20:03:30 +0200

    separate gem and hex package deps and injected remote

--- dummies/dummy1/config/deploy.rb
+++ dummies/dummy1/config/deploy.rb
@@ -2,7 +2,7 @@
 lock "~> 3.8.0"
 
 set :application, "dummy1"
-set :repo_url, "/tmp/working_paths/dummy1/dummies/dummy1/"
+set :repo_url, "__REMOTE__"
 
 set :repo_path, "dummy1_repo"
 set :build_path, "build_path"
--- test/support/dummy_app_case.ex
+++ test/support/dummy_app_case.ex
@@ -6,10 +6,10 @@ defmodule DummyAppCase do
       :name,
       :version,
 
+      :root_path, # root directory for the dummy test executions
       :hex_package_path, # where the dummy points to us as a mix dep
       :gem_path, # where the dummy points to us as a gem
 
-      :onartsipac_path, # where a copy of this entire repo is placed for this dummy app
       :remote, # git remote / dummy repository, used by build steps
       :capistrano_wd, # working dir when executing bundler and capistrano
 
@@ -40,46 +40,77 @@ defmodule DummyAppCase do
       dummy
     end
 
-    def prepare_onartsipac_path(%__MODULE__{name: name} = dummy, from: ".") do
-      onartsipac_path = ["/tmp/working_paths", name] |> Path.join |> Path.expand
+    def prepare_root_path(%__MODULE__{name: name} = dummy) do
+      root_path = ["/tmp/working_paths", name] |> Path.join |> Path.expand
 
-      File.rm_rf! onartsipac_path
-      File.mkdir_p! onartsipac_path
-      File.cp_r! ".", onartsipac_path
-      File.rm_rf! Path.join(onartsipac_path, ".git")
+      File.rm_rf! root_path
+      File.mkdir_p! root_path
 
-      hex_package_path = [onartsipac_path, "hex_package"] |> Path.join
+      %{ dummy | root_path: root_path }
+    end
+
+    def prepare_gem_and_hex_package(%__MODULE__{root_path: root_path} = dummy, from: ".") do
+      hex_package_path = [root_path, "hex_package"] |> Path.join
       File.mkdir_p! hex_package_path
 
+      from = System.cwd |> Path.expand
+
       Enamel.new
-      |> Enamel.command(~w{mix hex.build}, dir: onartsipac_path)
-      |> Enamel.command(~w{tar xf ../onartsipac-0.1.0.tar contents.tar.gz}, dir: hex_package_path)
+      |> Enamel.command(~w{mix hex.build})
+      |> Enamel.command(~w{tar xf #{from}/onartsipac-0.1.0.tar contents.tar.gz}, dir: hex_package_path)
       |> Enamel.command(~w{tar xf contents.tar.gz}, dir: hex_package_path)
       |> Enamel.run!
 
-      %{ dummy | onartsipac_path: onartsipac_path, hex_package_path: hex_package_path, gem_path: onartsipac_path }
+      gem_path = [root_path, "gem"] |> Path.join
+      File.mkdir_p! gem_path
+
+      Enamel.new
+      |> Enamel.command(~w{rake build})
+      |> Enamel.command(~w{tar xf #{from}/pkg/onartsipac-0.1.0.gem data.tar.gz}, dir: gem_path)
+      |> Enamel.command(~w{tar xf data.tar.gz}, dir: gem_path)
+      |> Enamel.run!
+
+      %{ dummy | hex_package_path: hex_package_path, gem_path: gem_path }
     end
 
-    def prepare_working_directory(%__MODULE__{name: name, onartsipac_path: onartsipac_path, hex_package_path: hex_package_path} = dummy) when is_binary(onartsipac_path) do
-      capistrano_wd = [onartsipac_path, "dummies", name] |> Path.join
+    def prepare_working_directory(
+        %__MODULE__{
+          name: name,
+          root_path: root_path,
+          hex_package_path: hex_package_path,
+          gem_path: gem_path
+        } = dummy) when is_binary(hex_package_path) and is_binary(gem_path) do
+      capistrano_wd = [root_path, "wd"] |> Path.join
 
-      Enamel.command([:find, capistrano_wd, ~w<-name mix.exs -exec sed -i
+      File.mkdir_p! capistrano_wd
+      File.cp_r! "./dummies/#{name}", capistrano_wd
+
+      # We use "future" knowledge here, because we keep setup
+      # simple by having the remote at the capistrano_wd
+      remote_path = capistrano_wd
+
+      Enamel.new
+      |> Enamel.command([:find, capistrano_wd, ~w<-name mix.exs -exec sed -i
         s|__HEX_PACKAGE_PATH__|#{hex_package_path}| {} ;>])
+      |> Enamel.command([:find, capistrano_wd, ~w<-name Gemfile -exec sed -i
+        s|__GEM_PATH__|#{gem_path}| {} ;>])
+      |> Enamel.command([:find, capistrano_wd, ~w<-name deploy.rb -exec sed -i
+        s|__REMOTE__|#{remote_path}| {} ;>])
       |> Enamel.command([:bundle], dir: capistrano_wd)
       |> Enamel.run!
 
-      %{ dummy | capistrano_wd: capistrano_wd }
+      %{ dummy | capistrano_wd: capistrano_wd, remote: remote_path }
     end
 
-    def prepare_remote(%__MODULE__{capistrano_wd: capistrano_wd} = dummy) when is_binary(capistrano_wd) do
-      Enamel.command([~w{git -C}, dummy.capistrano_wd, :init])
-      |> Enamel.command([~w{git -C}, dummy.capistrano_wd, ~w{config user.name}, "onartsipac test user"])
-      |> Enamel.command([~w{git -C}, dummy.capistrano_wd, ~w{config user.email onartsipac@example.com}])
-      |> Enamel.command([~w{git -C}, dummy.capistrano_wd, ~w{add .}])
-      |> Enamel.command([~w{git -C}, dummy.capistrano_wd, ~w{commit -m bogus}])
+    def initialize_remote(%__MODULE__{remote: remote} = dummy) when is_binary(remote) do
+      Enamel.command([~w{git -C}, remote, :init])
+      |> Enamel.command([~w{git -C}, remote, ~w{config user.name}, "onartsipac test user"])
+      |> Enamel.command([~w{git -C}, remote, ~w{config user.email onartsipac@example.com}])
+      |> Enamel.command([~w{git -C}, remote, ~w{add .}])
+      |> Enamel.command([~w{git -C}, remote, ~w{commit -m bogus}])
       |> Enamel.run!
 
-      %{ dummy | remote: capistrano_wd }
+      dummy
     end
   end
 
@@ -92,9 +123,10 @@ defmodule DummyAppCase do
         dummy =
           Dummy.new(name)
           |> Dummy.kill_stray_processes
-          |> Dummy.prepare_onartsipac_path(from: ".")
+          |> Dummy.prepare_root_path
+          |> Dummy.prepare_gem_and_hex_package(from: ".")
           |> Dummy.prepare_working_directory
-          |> Dummy.prepare_remote
+          |> Dummy.initialize_remote
 
         {:ok, %{dummy: dummy}}
       end
