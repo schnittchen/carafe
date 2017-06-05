073eba9c47a1f3ccd4028e0e6909c7152f051ac6
Thomas Stratmann <thomas.stratmann@9elements.com>
2017-06-05 18:41:38 +0200

    add source documentation to all current DSL functions

--- lib/carafe.rb
+++ lib/carafe.rb
@@ -7,6 +7,14 @@ load File.expand_path("../capistrano/tasks/node.rake", __FILE__)
 
 module Carafe
   module DSL
+    # Returns the build host, to be consumed by capistrano's `on` method like this:
+    # ```
+    # task :my_task do
+    #   on build_host do
+    #     ...
+    #   end
+    # end
+    # ```
     def build_host
       hosts = roles(:build)
 
@@ -16,18 +24,50 @@ module Carafe
       hosts.first
     end
 
+    # Returns the build path on the build host, can be used with capistrano's `within` method like this:
+    # ```
+    # task :my_task do
+    #   on build_host do |host|
+    #     within build_path do
+    #       ...
+    #     end
+    #   end
+    # end
+    # ```
     def build_path
       Pathname(fetch(:build_path) { raise "no :build_path configured" })
     end
 
+    # Returns the mix environment to be used when preparing and creating the release.
+    # Can be used with capistrano's `with` method like this (Note: `with` uppercases the name):
+    # ```
+    # task :my_task do
+    #   on build_host do |host|
+    #     within build_path do
+    #       with mix_env: mix_env do
+    #         ...
+    #       end
+    #     end
+    #   end
+    # end
+    # ```
     def mix_env
       fetch(:mix_env) { raise "set :mix_env in stage config!" }.to_s
     end
 
+    # Returns the path on the target hosts where releases are extracted and loaded from.
     def app_path
       Pathname(fetch(:app_path) { raise "set :app_path node path where the release is unpacked an run" })
     end
 
+    # Returns the target hosts, to be consumed by capistrano's `on` method like this:
+    # ```
+    # task :my_task do
+    #   on app_hosts do
+    #     ...
+    #   end
+    # end
+    # ```
     def app_hosts
       hosts = roles(:app)
 
@@ -36,10 +76,15 @@ module Carafe
       hosts
     end
 
+    # Returns the distillery environment to use, defaulting to the result of `mix_env`.
+    # The distillery environment is configured in `rel/config.exs`.
     def distillery_environment
       fetch(:distillery_environment) { mix_env }
     end
 
+    # Returns the distillery release to use, defaulting to the result of
+    # the capistrano variable named `:application`.
+    # The distillery release is configured in `rel/config.exs`.
     def distillery_release
       fetch(:distillery_release) {
         # the same defaulting as distillery does
