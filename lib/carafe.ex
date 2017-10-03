68a8b194e9c0a192869f19ea63ffad38b9b3b8fe
Thomas Stratmann <thomas.stratmann@9elements.com>
2017-10-03 21:56:24 +0200

    execute_elixir (carafe app)

--- lib/carafe.ex
+++ lib/carafe.ex
@@ -1,18 +1,29 @@
 defmodule Carafe do
   @moduledoc """
-  Documentation for Carafe.
+  This is the client-side interface for Carafe.
   """
 
   @doc """
-  Hello world.
-
-  ## Examples
-
-      iex> Carafe.hello
-      :world
-
+  Executes elixir code given as a string. Fails if the code returns :error or {:error, reason}.
+  Intended for communication with a running node.
   """
-  def hello do
-    :world
+  def execute_elixir(elixir) when is_list(elixir) do
+    elixir |> to_string |> execute_elixir
+  end
+  def execute_elixir(elixir) when is_binary(elixir) do
+    elixir
+    |> Code.eval_string
+    |> case do
+      :ok -> :ok
+      {:ok, _} -> :ok
+      :error = bad -> bad
+      {error, } = bad -> bad
+      other ->
+        if other, do: :ok, else: other
+    end
+    |> case do
+      :ok -> nil
+      other -> raise "execute_elixir failed: #{inspect other}"
+    end
   end
 end
